a7280518a49b1d7f038c6894089da312e66d6ef3
Ely <paysinger@gmail.com>
2022-01-30 06:55:50 -0800

    .

--- kolvir/aws/shared.py
+++ kolvir/aws/shared.py
@@ -3,7 +3,7 @@ from functools import lru_cache
 
 from boto3.session import Session
 
-AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION", "us-west-2")
+AWS_REGION = os.getenv("AWS_REGION", "us-west-2")
 BOTO_ENDPOINT_URL = os.getenv("BOTO_ENDPOINT_URL")
 
 
@@ -26,7 +26,7 @@ def get_session(cache_key=None):  # pylint:  disable=unused-argument
 
 @lru_cache
 def get_client(service, region_name=None, endpoint_url=None, cache_key=None):
-    kwargs = {"region_name": region_name or AWS_DEFAULT_REGION}
+    kwargs = {"region_name": region_name or AWS_REGION}
     if endpoint_url or BOTO_ENDPOINT_URL:
         kwargs["endpoint_url"] = endpoint_url or BOTO_ENDPOINT_URL
     return get_session(cache_key=cache_key).client(service, **kwargs)
